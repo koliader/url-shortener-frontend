063904db8a3c7305a8b4df7c51a0df23ced81f09
Mykola Derevianko <kyderevyanko@gamil.com>
2024-04-05 13:24:48 +0300

    Using getServerSideProps instead of simple client req

--- pages/code/[code].tsx
+++ pages/code/[code].tsx
@@ -1,21 +1,37 @@
 import { IUrlRes } from "@/app/components/screens/Home/HomeForm/types";
 import axios from "@/app/helpers/axios";
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import { NextRouter } from "next/router";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import ErrorPage from "next/error";
 
-const UrlPage: NextPage = () => {
+export interface IUrlPageServerSideProps {
+  url: string | null;
+  notFound: boolean;
+}
+
+const UrlPage: NextPage<IUrlPageServerSideProps> = ({ url, notFound }) => {
   const router: NextRouter = useRouter();
   useEffect(() => {
-    if (router.isReady) {
-      const req = async () => {
-        const url = await axios.get<IUrlRes>(`/urls/${router.query.code}`);
-        router.push(url.data.url);
-      };
-      req();
+    if (!notFound) {
+      router.push(url!);
     }
   }, [router.isReady]);
-  return <></>;
+  return <>{notFound && <ErrorPage statusCode={404} />}</>;
 };
 export default UrlPage;
+export const getServerSideProps: GetServerSideProps<
+  IUrlPageServerSideProps
+> = async (ctx) => {
+  try {
+    const url = await axios.get<IUrlRes>(`/urls/${ctx.params!.code}`);
+    return { props: { url: url.data.url, notFound: false } };
+  } catch (error: any) {
+    console.log(error.response.status);
+    if (error.response.status === 404) {
+      return { props: { url: null, notFound: true } };
+    }
+    return { props: { url: null, notFound: false } };
+  }
+};
