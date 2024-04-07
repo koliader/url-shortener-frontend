6e10bf02d5519d646da231b2bd0d6c9b340960c1
Mykola Derevianko <kyderevyanko@gamil.com>
2024-04-07 14:59:42 +0300

    Fixed navbar

--- app/components/shared/Layouts/Layout/Layout.tsx
+++ app/components/shared/Layouts/Layout/Layout.tsx
@@ -4,11 +4,11 @@ import Footer from "@/app/components/ui/Footer/Footer";
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <NavBar />
-      <main className="mt-3">{children}</main>
+      <main className="flex-grow overflow-auto mt-3 mb-16">{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
